591f0d66d329cd91ccbe311739fd0054d02badd5
Erling Lothe <el@pondi.no>
2024-05-09 00:15:51 +0200

    Initial code

---
+++ resources/js/Pages/QuickDrop.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <Head title="Welcome" />
+    <GuestLayout>
+        <Vueform
+    validate-on="step|change"
+  >
+    <StaticElement
+      name="h2"
+      tag="h2"
+      content="Drop your files"
+      align="center"
+    />
+    <StaticElement
+      name="divider"
+      tag="hr"
+    />
+    <MultifileElement
+      name="multifile"
+      :drop="true"
+      :upload-temp-endpoint="{
+        url: '/store',
+      }"
+      :clickable="false"
+      :remove-temp-endpoint="{
+        url: '/deltemp',
+      }"
+      :remove-endpoint="{
+        url: '/delete',
+      }"
+      accept=".tar,.bz,.gz,.zip,.7z,.bz2,.doc,.docx,.xls,.xlsx,.ppt,.pptx,.pdf,.txt"
+      :controls="{
+        remove: false,
+      }"
+    />
+  </Vueform>
+
+</GuestLayout>
+  </template>
+
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+</script>
